0a809ce9af07c53279b048489175c80a50c1ca44
Dean Lunn <dean.lunn82@gmail.com>
2023-04-06 18:06:35 -0600

    first

---
+++ forms.py
@@ -0,0 +1,78 @@
+"""Forms for flask-feedback."""
+
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
+from flask_wtf import FlaskForm
+from models import User
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    def validate(self, extra_validators=None):
+        """Validate form."""
+
+        initial_validation = super(RegisterForm, self).validate()
+
+        if not initial_validation:
+            return False
+
+        existing_user = User.query.filter_by(username=self.username.data).first()
+
+        if existing_user:
+            self.username.errors.append("Username already taken")
+            return False
+
+        return True
+
+
+
+class FeedbackForm(FlaskForm):
+    """Add feedback form."""
+
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    content = StringField(
+        "Content",
+        validators=[InputRequired()],
+    )
+
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
